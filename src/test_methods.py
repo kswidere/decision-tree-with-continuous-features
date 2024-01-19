03303f6211aa0bae0dc67fdc9d8da30021275ee8
jkowalc5 <01169190@pw.edu.pl>
2024-01-19 16:29:38 +0100

    Dodałem 3 metody testowania oraz zmieniłem listy na pd.dataframe

--- src/test_methods.py
+++ src/test_methods.py
@@ -1,64 +1,81 @@
+from math import log2
+import math
+import pandas as pd
+from copy import deepcopy
+import numpy as np
+
+
 class TestMethod:
-    def choose_test(self):
-        raise NotImplementedError(
-            "This method needs to be overridden in subclasses")
-
-    def find_possible_tests(self, train_dataset):
-        raise NotImplementedError(
-            "This method needs to be overridden in subclasses")
-
-
-class EqualFrequency(TestMethod):
-    def __init__(self):
-        self.possible_tests = []
-
     def choose_test(self, train_dataset):
-        return self.possible_tests.pop()
+        """
+        Wybiera najlepszy test dla danego zbioru danych.
+
+        Args:
+            dataset: Zbiór danych w postaci pandas DataFrame z kolumną 'target'.
+
+        Returns:
+            Funkcja testująca, która przyjmuje wiersz danych i zwraca wartość logiczną.
+        """
+        raise NotImplementedError(
+            "This method needs to be overridden in subclasses")
 
     def find_possible_tests(self, train_dataset):
-        num_attributes = len(train_dataset[0])-1
+        """
+        Znajduje możliwe testy dla danego zbioru danych.
 
-        for attribute_index in range(num_attributes):
-            sorted_dataset = sorted(
-                train_dataset, key=lambda x: x[attribute_index])
-            mid_index = len(sorted_dataset) // 2
-            threshold = (sorted_dataset[mid_index - 1][attribute_index] +
-                         sorted_dataset[mid_index][attribute_index]) / 2
-            self.possible_tests.append(
-                lambda x: x[attribute_index] <= threshold)
-        return self.possible_tests
+        Args:
+            dataset: Zbiór danych w postaci pandas DataFrame z kolumną 'target'.
 
-# class EqualFrequency(TestMethod):
-#     def choose_test(self, train_dataset):
-#         # Assuming train_dataset is a list of numerical values
-#         sorted_dataset = sorted(train_dataset)
-#         median_index = len(sorted_dataset) // 2
-#         threshold = sorted_dataset[median_index]
+        Returns:
+            Lista funkcji testujących, które przyjmują wiersz danych i zwracają wartość logiczną.
+        """
+        raise NotImplementedError(
+            "This method needs to be overridden in subclasses")
 
-#         def test(row):
-#             return row <= threshold
 
-#         return test
+class InformationGainTest:
+    def entropy(self, targets):
+        value, counts = np.unique(targets, return_counts=True)
+        probabilities = counts / counts.sum()
+        entropy = -sum(probabilities * np.log2(probabilities))
+        return entropy if not np.isnan(entropy) else 0
 
-#     def find_possible_tests(self, train_dataset):
-#         # Assuming train_dataset is a list of lists of numerical values
-#         possible_tests = []
-#         for feature_index in range(len(train_dataset[0])):
-#             # Extract the feature column
-#             feature_values = [row[feature_index] for row in train_dataset]
-#             # Sort the feature values
-#             sorted_feature_values = sorted(feature_values)
-#             # Find the median index
-#             median_index = len(sorted_feature_values) // 2
-#             # Use the median value as the threshold
-#             threshold = sorted_feature_values[median_index]
-#             # Define the test function
+    def information_gain(self, left_targets, right_targets, current_entropy):
+        p_left = len(left_targets) / (len(left_targets) + len(right_targets))
+        p_right = 1 - p_left
+        gain = current_entropy - p_left * \
+            self.entropy(left_targets) - p_right * self.entropy(right_targets)
+        return gain
 
-#             def test(row, threshold=threshold, feature_index=feature_index):
-#                 return row[feature_index] <= threshold
-#             # Add the test function to the list of possible tests
-#             possible_tests.append(test)
-#         return possible_tests
+    def find_possible_tests(self, data):
+        possible_tests = []
+        for feature in data.columns:
+            unique_values = sorted(data[feature].unique())
+            for i in range(len(unique_values) - 1):
+                threshold = (unique_values[i] + unique_values[i + 1]) / 2
+                possible_tests.append(lambda row, threshold=threshold,
+                                      feature=feature: row[feature] <= threshold)
+        return possible_tests
+
+    def choose_test(self, data):
+        targets = data['target']
+        current_entropy = self.entropy(targets)
+        best_gain = -1
+        best_test = None
+
+        for test in self.find_possible_tests(data.drop(columns='target')):
+            left_targets = targets[data.drop(
+                columns='target').apply(test, axis=1)]
+            right_targets = targets[~data.drop(
+                columns='target').apply(test, axis=1)]
+            gain = self.information_gain(
+                left_targets, right_targets, current_entropy)
+
+            if gain > best_gain:
+                best_gain = gain
+                best_test = test
+
+        return best_test
 
 
 class EqualWidth(TestMethod):
@@ -69,9 +86,100 @@ class KMeans(TestMethod):
     pass
 
 
-class Gini(TestMethod):
-    pass
+class GiniImpurityTest(TestMethod):
+    def choose_test(self, train_dataset):
+        best_test = None
+        best_gini = float('inf')
+
+        for test in self.find_possible_tests(train_dataset):
+            gini = self.calculate_gini(train_dataset, test)
+            if gini < best_gini:
+                best_gini = gini
+                best_test = test
+        print(f"wybieram test {best_test}")
+        return best_test
+
+    def find_possible_tests(self, train_dataset):
+        possible_tests = []
+
+        for column in train_dataset.columns:
+            if column != 'target':
+                sorted_values = sorted(train_dataset[column].unique())
+                for i in range(len(sorted_values) - 1):
+                    average = (sorted_values[i] + sorted_values[i + 1]) / 2
+                    possible_tests.append(lambda row, threshold=average,
+                                          feature=column: row[feature] > threshold)
+        print("found possible tests")
+        return possible_tests
+
+    def calculate_gini(self, train_dataset, test):
+        left_data = train_dataset[train_dataset.apply(test, axis=1)]
+        right_data = train_dataset[~train_dataset.apply(test, axis=1)]
+
+        left_target_counts = left_data['target'].value_counts()
+        right_target_counts = right_data['target'].value_counts()
+
+        left_gini = self.calculate_gini_for_target_counts(left_target_counts)
+        right_gini = self.calculate_gini_for_target_counts(right_target_counts)
+
+        return (len(left_data) / len(train_dataset)) * left_gini + (len(right_data) / len(train_dataset)) * right_gini
+
+    def calculate_gini_for_target_counts(self, target_counts):
+        """
+        Oblicza współczynnik Gini dla danych liczb wystąpień klas.
+        Zwraca target_counts: Liczby wystąpień klas w postaci pandas Series.
+        """
+        gini = 1 - sum((count / target_counts.sum())
+                       ** 2 for count in target_counts)
+        return gini
 
 
-class InfGain(TestMethod):
-    pass
+class EqualFrequencyTest(TestMethod):
+    def choose_test(self, train_dataset):
+        best_test = None
+        best_info_gain = -np.inf
+
+        possible_tests = self.find_possible_tests(train_dataset)
+
+        for test in possible_tests:
+            info_gain = self.calculate_information_gain(test, train_dataset)
+            if info_gain > best_info_gain:
+                best_info_gain = info_gain
+                best_test = test
+        print(f"wybieram test {best_test}")
+        return best_test
+
+    def find_possible_tests(self, train_dataset):
+        possible_tests = []
+
+        for column in train_dataset.columns:
+            if column != 'target':
+                # Sort the values and split into bins with approximately equal number of observations
+                sorted_values = sorted(train_dataset[column].dropna().unique())
+                n_bins = int(np.sqrt(len(sorted_values)))
+                bins = pd.qcut(sorted_values, q=n_bins,
+                               duplicates='drop', retbins=True)[1]
+
+                # Create tests for the thresholds between bins
+                for i in range(1, len(bins)):
+                    threshold = bins[i]
+                    possible_tests.append(
+                        lambda row, column=column, threshold=threshold: row[column] <= threshold)
+        print("found possible tests")
+        return possible_tests
+
+    def calculate_information_gain(self, test, dataset):
+        entropy_before = self.calculate_entropy(dataset['target'])
+
+        passed = dataset[dataset.apply(test, axis=1)]
+        failed = dataset[~dataset.apply(test, axis=1)]
+
+        entropy_after = (len(passed) / len(dataset)) * self.calculate_entropy(passed['target']) + \
+                        (len(failed) / len(dataset)) * \
+            self.calculate_entropy(failed['target'])
+
+        return entropy_before - entropy_after
+
+    def calculate_entropy(self, series):
+        probabilities = series.value_counts(normalize=True)
+        return -np.sum(probabilities * np.log2(probabilities))
--- src/tree.py
+++ src/tree.py
@@ -1,6 +1,9 @@
 import pandas as pd
-from test_methods import EqualFrequency
+from test_methods import InformationGainTest, EqualFrequencyTest, GiniImpurityTest
 from copy import deepcopy
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn import tree as sktree
 
 
 class TreeNode:
@@ -15,7 +18,7 @@ class TreeNode:
 
     def predict(self, dataset):
         predicted_targets = []
-        for row in dataset:
+        for _, row in dataset.iterrows():
             prediction = self.predict_row(row)
             predicted_targets.append(prediction)
         return predicted_targets
@@ -30,19 +33,15 @@ class TreeNode:
 
 
 class DecisionTree:
-    def __init__(self, train_dataset, train_targets, test_method,
+    def __init__(self, train_data, test_method,
                  default_target=None, possible_tests=None):
-        self.train_dataset = train_dataset
-        self.train_targets = train_targets
+        self.train_data = train_data
         self.test_method = test_method
         self.default_target = (self.find_default_target()
                                if default_target is None else default_target)
         self.possible_tests = (self.find_possible_tests()
                                if possible_tests is None else possible_tests)
         self.root: TreeNode = self.fit()
-
-    def __str__(self):
-        pass
 
     def fit(self):
         leaf_target = self.leaf_target()
@@ -54,47 +53,40 @@ class DecisionTree:
         test = self.choose_test()
         root = TreeNode(False, test=deepcopy(test))
         default_target = self.find_default_target()
-        left_dataset, left_targets = self.find_new_dataset(test, False)
-        right_dataset, right_targets = self.find_new_dataset(test, True)
+        left_data = self.find_new_data(test, False)
+        right_data = self.find_new_data(test, True)
 
-        subtree_left = DecisionTree(left_dataset, left_targets,
-                                    self.test_method, default_target,
-                                    self.possible_tests)
-
-        subtree_right = DecisionTree(right_dataset, right_targets,
-                                     self.test_method, default_target,
-                                     self.possible_tests)
+        subtree_left = DecisionTree(
+            left_data, self.test_method, default_target, self.possible_tests)
+        subtree_right = DecisionTree(
+            right_data, self.test_method, default_target, self.possible_tests)
 
         root.left = subtree_left.root
         root.right = subtree_right.root
         return root
 
     def leaf_target(self):
-        if len(set(self.train_targets)) == 1:
-            return self.train_targets[0]
-        elif not self.train_dataset:
+        if len(self.train_data['target'].unique()) == 1:
+            return self.train_data['target'].iloc[0]
+        elif self.train_data.empty:
             return self.default_target
         elif not self.possible_tests:
             return self.find_default_target()
         return None
 
     def choose_test(self):
-        return self.test_method.choose_test(self.train_dataset)
+        return self.test_method.choose_test(self.train_data)
 
     def find_default_target(self):
-        return max(set(self.train_targets), key=self.train_targets.count)
+        return self.train_data['target'].mode()[0]
 
     def find_possible_tests(self):
-        return self.test_method.find_possible_tests(self.train_dataset)
+        return self.test_method.find_possible_tests(self.train_data)
 
-    def find_new_dataset(self, test, test_passed: bool):
-        new_dataset = deepcopy(self.train_dataset)
-        new_targets = deepcopy(self.train_targets)
-        for idx, row in enumerate(self.train_dataset):
-            if test(row) is not test_passed:
-                del new_targets[new_dataset.index(row)]
-                new_dataset.remove(row)
-        return (new_dataset, new_targets)
+    def find_new_data(self, test, test_passed: bool):
+        new_data = deepcopy(self.train_data)
+        new_data = new_data[new_data.apply(test, axis=1) == test_passed]
+        return new_data
 
     def predict(self, dataset):
         return self.root.predict(dataset)
@@ -102,17 +94,15 @@ class DecisionTree:
 
 if __name__ == "__main__":
     data = pd.read_csv('src/diabetes.csv')
+    data = data.rename(columns={'Outcome': 'target'})
+    iris_data = pd.read_csv('src/Iris.csv')
+    iris_data = data.rename(columns={'Species': 'target'})
 
-    # Podział danych na cechy i etykiety
-    features = data.drop('Outcome', axis=1)
-    labels = data['Outcome']
-
-    # Konwersja na listy
-    features_list = features.values.tolist()
-    labels_list = labels.values.tolist()
-
-    tree = DecisionTree(deepcopy(features_list),
-                        deepcopy(labels_list),
-                        EqualFrequency())
-    predictions = tree.predict(features_list)
-    print(predictions)
+    tree = DecisionTree(deepcopy(data),
+                        GiniImpurityTest())
+    tree2 = sktree.DecisionTreeClassifier()
+    tree2.fit(data.drop(columns='target'), data['target'])
+    y2_pred = tree2.predict(data.drop(columns='target'))
+    y_pred = tree.predict(data)
+    # print(y_pred)
+    print(classification_report(data['target'], y_pred))
